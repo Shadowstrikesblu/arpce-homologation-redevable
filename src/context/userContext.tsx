c48dc691df5391826425d7882801bb8dd75062c6
k2pme <cmantsila0@gmail.com>
2025-11-20 11:25:00 +0100

    ajout du context user

---
+++ src/context/userContext.tsx
@@ -0,0 +1,78 @@
+// app/context/UserContext.tsx
+"use client";
+
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+  PropsWithChildren,
+} from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { inherits } from "util";
+import { Client } from "@/lib/interfaces/models.interface";
+
+export interface User extends Client {}
+
+interface UserContextValue {
+  user: User | null;
+  setUser: Dispatch<SetStateAction<User | null>>;
+}
+
+const UserContext = createContext<UserContextValue | undefined>(undefined);
+
+const TOKEN_KEY = process.env["NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY"]
+
+export function UserProvider({ children }: PropsWithChildren) {
+
+    const [user, setUser] = useState<User | null>(null);
+    const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+    const router = useRouter();
+    const pathname = usePathname();
+
+    useEffect(() => {
+
+    if (typeof window === "undefined") return;
+
+    const token = localStorage.getItem(TOKEN_KEY);
+
+    if (!token) {
+        // pas de token -> redirection vers login avec la page suivante en query
+        const next = encodeURIComponent(pathname || "/");
+        router.replace(`/auth/login?next=${next}`);
+        return;
+    }
+
+
+    if (!user) {
+        const next = encodeURIComponent(pathname || "/");
+        router.replace(`/auth/login?next=${next}`);
+        return;
+    }
+
+    setIsCheckingAuth(false);
+
+    }, [router, pathname, user]);
+
+
+    if (isCheckingAuth) {
+        return null; 
+    }
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export function useUser(): [User | null, Dispatch<SetStateAction<User | null>>] {
+    const context = useContext(UserContext);
+    if (!context) {
+    throw new Error("useUser must be used inside a <UserProvider />");
+    }
+    return [context.user, context.setUser];
+}
