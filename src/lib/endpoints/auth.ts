b739fc3037ca64df5ce3c53aae2991f002125434
SylviaMauricette <ngassakisylvia@icloud.com>
2025-11-20 11:52:11 +0100

    n

--- src/lib/endpoints/auth.ts
+++ src/lib/endpoints/auth.ts
@@ -19,17 +19,13 @@ export const auth = {
 
     login : async (input : loginInt)=>{
         const {data} = await axiosClient.post("/api/auth/login", input)
-        
+
         return data 
     },
 
     register : async (input : RegisterInt)=>{
 
-<<<<<<< HEAD
-        const {data} = await axiosClient.post("/api/auth/register", {...input})
-=======
         const {data} = await axiosClient.post("/api/auth/register", input)
->>>>>>> 229e06e31439cc338b7d7c16f7695825836f0428
 
         return data 
     }
