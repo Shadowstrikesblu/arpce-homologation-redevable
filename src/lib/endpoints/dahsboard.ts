bf064643fe722341f34d308d8c3f41cbc9e420a1
Simplice <simplicebissemo11@gmail.com>
2025-11-20 12:10:52 +0100

    ajout du end-point dashboard

---
+++ src/lib/endpoints/dahsboard.ts
@@ -0,0 +1,61 @@
+import axiosClient from "../utils/axiosBase";
+
+export interface DashboardStatsInt {
+  total: number;
+  success: number;
+  failed: number;
+  inProgress: number;
+  pendingPayments: number;
+}
+
+export interface StatutInt {
+  id: string;
+  code: string;
+  libelle: string;
+}
+
+export interface DemandeInt {
+  id: string;
+  idDossier: string;
+  numeroDemande: string;
+  equipement: string;
+  modele: string;
+  marque: string;
+}
+
+export interface RecentFileInt {
+  id: string;
+  idClient: string;
+  idStatut: string;
+  idModeReglement: string;
+  dateOuverture: string; // ou Date si tu veux parser
+  numero: string;
+  libelle: string;
+  statut: StatutInt;
+  demandes: DemandeInt[];
+}
+
+export interface PendingPaymentsInt {
+  id: string; // UUID
+  numeroDemande: string | null; // nullable
+  montant: number; // double
+  dateEcheance: string; // ISO date-time
+  modeReglementLibelle: string;
+}
+
+export const dashboardStats = {
+  getFile: async (): Promise<DashboardStatsInt> => {
+    const { data } = await axiosClient.get("/api/dossiers/apercu");
+    return data;
+  },
+
+  getRecentFile: async (): Promise<RecentFileInt> => {
+    const { data } = await axiosClient.get("/api/dossiers/recents");
+    return data;
+  },
+
+  getPendingPayments: async (dossierId: number): Promise<PendingPaymentsInt> => {
+    const { data } = await axiosClient.get(`/api/dossiers/${dossierId}/paiement-en-attente`);
+    return data;
+  },
+};
